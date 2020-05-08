dde881c4c4c2c6070f279dfbaf5d15eec4398d64
yaofrankie <15900617310@163.com>
2020-05-08 16:36:14 +0800

    Play HashMap

---
+++ src/main/java/com/frankie/demo/hashTable/HashTableTest.java
@@ -0,0 +1,27 @@
+package com.frankie.demo.hashTable;
+
+import org.omg.CORBA.INTERNAL;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author: Yao Frankie
+ * @date: 2020/5/8 15:38
+ */
+public class HashTableTest {
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> m = new LinkedHashMap<>(10, 0.75f, true);
+        m.put(3, 11);
+        m.put(1, 12);
+        m.put(5, 23);
+        m.put(2, 22);
+        m.put(3, 26);
+        m.get(5);
+        for (Map.Entry<Integer, Integer> e : m.entrySet()) {
+            System.out.println(e.getKey());
+        }
+    }
+}
