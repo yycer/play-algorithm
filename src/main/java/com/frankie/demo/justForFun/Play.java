485b754fcec6eb56ed9a1a91a7f63bd874d33db2
yycer <15900617310@163.com>
2020-05-28 23:18:12 +0800

    KO 118. Pascal's Triangle

--- src/main/java/com/frankie/demo/justForFun/Play.java
+++ src/main/java/com/frankie/demo/justForFun/Play.java
@@ -1,5 +1,10 @@
 package com.frankie.demo.justForFun;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author: Yao Frankie
  * @date: 2020/5/26 9:44
@@ -7,7 +12,64 @@ package com.frankie.demo.justForFun;
 public class Play {
 
     public static void main(String[] args) {
-        p509();
+//        p509();
+        p118();
+    }
+
+    /**
+     * 118. Pascal's Triangle
+     */
+    private static void p118() {
+        int numRows = 5;
+//        List<List<Integer>> ans = generate(numRows);
+        List<List<Integer>> ans = generateOptimize1(numRows);
+        for (List<Integer> child: ans){
+            System.out.println(child);
+        }
+    }
+
+    private static List<List<Integer>> generate(int numRows) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        if (numRows == 0) return ans;
+        if (numRows == 1){
+            ans.add(Collections.singletonList(1));
+            return ans;
+        }
+        if (numRows == 2){
+            ans.add(Arrays.asList(1));
+            ans.add(Arrays.asList(1, 1));
+            return ans;
+        }
+        ans.add(Arrays.asList(1));
+        ans.add(Arrays.asList(1, 1));
+        for (int i = 3; i <= numRows; i++){
+            ArrayList<Integer> tmpList = new ArrayList<>();
+            tmpList.add(1);
+            for (int j = 1; j < i - 1; j++){
+                tmpList.add(ans.get(i - 2).get(j - 1) + ans.get(i - 2).get(j));
+            }
+            tmpList.add(1);
+            ans.add(tmpList);
+        }
+        return ans;
+    }
+
+    private static List<List<Integer>> generateOptimize1(int numRows) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < numRows; i++){
+            ArrayList<Integer> tmp = new ArrayList<>(Arrays.asList(1));
+            // Think about the second layer: [1, 1]
+            for (int j = 1; j < i; j++){
+                // a[i][j] = a[i-1][j-1] + a[i-1][j]
+                tmp.add(ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
+            }
+            // Think about the first layer: [1]
+            if (i > 0) tmp.add(1);
+            ans.add(tmp);
+        }
+        return ans;
     }
 
     private static void p509() {
