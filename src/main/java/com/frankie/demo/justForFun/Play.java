f3943f21bb184229c7daf848dfc1ae7964a8e937
yycer <15900617310@163.com>
2020-05-30 10:18:13 +0800

    maxSubArray_2020_0529()

--- src/main/java/com/frankie/demo/justForFun/Play.java
+++ src/main/java/com/frankie/demo/justForFun/Play.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
  * @author: Yao Frankie
@@ -14,8 +15,48 @@ public class Play {
     public static void main(String[] args) {
 //        p509();
 //        p118();
-//        p53();
-        p189(); // Rotate Array
+        p53();
+//        p189(); // Rotate Array
+//        p268(); // Missing Number
+    }
+
+    private static void p268() {
+        int[] nums = {3, 0, 1};
+//        int ret1 = missingNumber1(nums);
+//        int ret1 = missingNumberUsingXOR(nums);
+        int ret1 = missingNumberUsingBS(nums);
+        System.out.println(ret1);
+    }
+
+    private static int missingNumberUsingBS(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+        int lo  = 0, hi = len - 1;
+        while (lo <= hi){
+            int mid = (lo + hi) >>> 1;
+            if (nums[mid] <= mid){
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lo;
+    }
+
+    private static int missingNumberUsingXOR(int[] nums) {
+        int len = nums.length;
+        int ans = len;
+        for (int i = 0; i < len; i++){
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+
+    private static int missingNumber1(int[] nums) {
+        int len = nums.length;
+//        return len * (len + 1) / 2 - Arrays.stream(nums).sum();
+        return ((len * (len + 1)) >> 1) - IntStream.of(nums).sum();
     }
 
     /**
@@ -76,8 +117,22 @@ public class Play {
 
     private static void p53() {
         int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int ret1 = maxSubArray(nums);
+//        int ret1 = maxSubArray(nums);
+        int ret1 = maxSubArray_2020_0529(nums);
         System.out.println(ret1);
+    }
+
+    private static int maxSubArray_2020_0529(int[] nums) {
+        // [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+        //  -2  1  -2  4   3  5  6   1  5
+        int len  = nums.length;
+        int[] dp = new int[len];
+
+        dp[0] = nums[0];
+        for (int i = 1; i < len; i++){
+            dp[i] = nums[i] + Math.max(dp[i - 1], 0);
+        }
+        return IntStream.of(dp).max().orElse(0);
     }
 
     private static int maxSubArray(int[] nums) {
