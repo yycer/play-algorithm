0f5dc91d58468e72df8e2f067da1e270007e2152
yaofrankie <15900617310@163.com>
2020-06-01 13:23:01 +0800

    Review 300. Longest Increasing Subsequence

--- src/main/java/com/frankie/demo/justForFun/Play_23_2020_0601.java
+++ src/main/java/com/frankie/demo/justForFun/Play_23_2020_0601.java
@@ -1,6 +1,7 @@
 package com.frankie.demo.justForFun;
 
 import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * @author: Yao Frankie
@@ -11,7 +12,36 @@ public class Play_23_2020_0601 {
     public static void main(String[] args) {
 //        p413(); // 413. Arithmetic Slices
 //        p343(); // 343. Integer Break
-        p279(); // 279. Perfect Squares
+//        p279(); // 279. Perfect Squares
+        p300(); // 300. Longest Increasing Subsequence
+    }
+
+    private static void p300() {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        int ret1 = lengthOfLIS(nums);
+        System.out.println(ret1);
+    }
+
+    private static int lengthOfLIS(int[] nums) {
+        int len  = nums.length;
+        // Think about []
+        if (len == 0) return 0;
+        int[] dp = new int[len];
+        dp[0] = 1;
+        for (int i = 1; i < len; i++){
+            int cur = nums[i];
+            int end = i;
+            // Think about the same element, eg: [4, 10, 4, 3, 8, 9]
+            // int max = dp[i - 1];
+            int max = 1;
+            while (--end >= 0){
+                if (cur > nums[end]){
+                    max = Math.max(max, dp[end] + 1);
+                }
+            }
+            dp[i] = max;
+        }
+        return IntStream.of(dp).max().orElse(0);
     }
 
     private static void p279() {
