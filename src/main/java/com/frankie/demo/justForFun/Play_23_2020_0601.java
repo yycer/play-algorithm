006e7ada66ad7842b03124921a5f276e7b0ecfab
yaofrankie <15900617310@163.com>
2020-06-01 14:50:54 +0800

    KO 376. Wiggle Subsequence

--- src/main/java/com/frankie/demo/justForFun/Play_23_2020_0601.java
+++ src/main/java/com/frankie/demo/justForFun/Play_23_2020_0601.java
@@ -13,11 +13,100 @@ public class Play_23_2020_0601 {
 //        p413(); // 413. Arithmetic Slices
 //        p343(); // 343. Integer Break
 //        p279(); // 279. Perfect Squares
-        p300(); // 300. Longest Increasing Subsequence
+//        p300(); // 300. Longest Increasing Subsequence
+        p376(); // 376. Wiggle Subsequence
+    }
+
+    /**
+     * https://leetcode.com/problems/wiggle-subsequence/discuss/84843/Easy-understanding-DP-solution-with-O(n)-Java-version
+     */
+    private static void p376() {
+//        int[] nums = {1, 17, 5, 10, 13, 15, 10, 5, 16, 8};
+//        int[] nums = {0, 0, 0};
+//        int[] nums = {0, 0};
+//        int[] nums = {1, 2, 3, 4, 5};
+        int[] nums = {3, 3, 3, 2, 5};
+//        int ret1 = wiggleMaxLengthNotWork(nums);
+//        int ret1 = wiggleMaxLengthUpDown(nums);
+        int ret1 = wiggleMaxLengthUpDownOptimize(nums);
+        System.out.println(ret1);
+    }
+
+    private static int wiggleMaxLengthUpDown(int[] nums) {
+
+        int len = nums.length;
+        // Think about []
+        if (len == 0) return 0;
+        int[] riseDp = new int[len];
+        int[] dropDp = new int[len];
+
+        riseDp[0] = 1;
+        dropDp[0] = 1;
+
+        for (int i = 1; i < len; i++){
+            int pre = nums[i - 1];
+            int cur = nums[i];
+            if (cur > pre){
+                riseDp[i] = dropDp[i - 1] + 1;
+                dropDp[i] = dropDp[i - 1];
+            } else if (cur < pre){
+                dropDp[i] = riseDp[i - 1] + 1;
+                riseDp[i] = riseDp[i - 1];
+            } else {
+                riseDp[i] = riseDp[i - 1];
+                dropDp[i] = dropDp[i - 1];
+            }
+        }
+        return Math.max(riseDp[len - 1], dropDp[len - 1]);
+    }
+
+    private static int wiggleMaxLengthUpDownOptimize(int[] nums) {
+
+        int len = nums.length;
+        // Think about []
+        if (len == 0) return 0;
+        int up = 1, down = 1;
+
+        for (int i = 1; i < len; i++){
+            int cur = nums[i];
+            int pre = nums[i - 1];
+            if (cur > pre){
+                up   = down + 1;
+            } else if (cur < pre){
+                down = up + 1;
+            }
+        }
+        return Math.max(up, down);
+    }
+
+    /**
+     * nums = [0, 0]          return 1;
+     * nums = [0, 0, 0]       return 1;
+     * nums = [1, 2, 3, 4, 5] return 2;
+     * nums = [3, 3, 3, 2, 5] return 2;
+     */
+    private static int wiggleMaxLengthNotWork(int[] nums) {
+
+        int len  = nums.length;
+        if (len < 2) return len;
+        int end  = 1;
+        int[] dp = new int[len];
+        dp[1]    = 2;
+
+        for (int i = 2; i < len; i++){
+            int cur = nums[i];
+            if ((cur - nums[end]) * (nums[end] - nums[end - 1]) < 0){
+                dp[i] = dp[end] + 1;
+                end   = i;
+            }
+        }
+        dp[1] = 1;
+        long distinctCount = Arrays.stream(nums).distinct().count();
+        return dp[len - 1] > 0 ? dp[len - 1] : distinctCount == 1 ? 1 : 2;
     }
 
     private static void p300() {
-        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        int[] nums = {10,  9, 2, 5, 3, 7, 101, 18};
         int ret1 = lengthOfLIS(nums);
         System.out.println(ret1);
     }
