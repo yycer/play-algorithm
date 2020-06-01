023b1c08912d7c4ce61fd5a2c94706dc2c497fdf
yaofrankie <15900617310@163.com>
2020-06-01 11:35:31 +0800

    Review 343 and 279

--- src/main/java/com/frankie/demo/justForFun/Play_23_2020_0601.java
+++ src/main/java/com/frankie/demo/justForFun/Play_23_2020_0601.java
@@ -9,8 +9,94 @@ import java.util.Arrays;
 public class Play_23_2020_0601 {
 
     public static void main(String[] args) {
-        p413(); // 413. Arithmetic Slices
+//        p413(); // 413. Arithmetic Slices
+//        p343(); // 343. Integer Break
+        p279(); // 279. Perfect Squares
     }
+
+    private static void p279() {
+        int n = 12;
+//        int ret1 = numSquares(n);
+        int ret1 = numSquaresOptimize(n);
+        System.out.println(ret1);
+    }
+
+    /**
+     * 118 ms
+     */
+    private static int numSquares(int n) {
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++){
+            for (int j = 1; j <= (int) Math.sqrt(i); j++){
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    private static int numSquaresOptimize(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++){
+            int min  = Integer.MAX_VALUE;
+            int sqrt = (int) Math.sqrt(i);
+            while (sqrt > 0){
+                min = Math.min(min, dp[i - sqrt * sqrt] + 1);
+                sqrt--;
+            }
+            dp[i] = min;
+        }
+        return dp[n];
+    }
+
+
+    private static void p343() {
+        int n = 10;
+        integerBreak(n);
+    }
+
+    private static int integerBreak(int n) {
+
+        if (n == 1) return 0;
+        if (n == 2) return 1;
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        dp[3] = 2;
+
+        for (int i = 4; i <= n; i++){
+            for (int j = 2; j <= (i >> 1); j++){
+                // Think about n = 4, dp[2] = 1;
+                // max3(0, 2 * dp[2], 2 * (4 - 2));
+                dp[i] = max3(dp[i], j * dp[i - j], j * (i - j));
+            }
+        }
+        return dp[n];
+    }
+
+    private static int integerBreakOptimize(int n) {
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++){
+            for (int j = 1; j <= (i >> 1); j++){
+                // Think about n = 4, dp[2] = 1;
+                // max3(0, 2 * dp[2], 2 * (4 - 2));
+                dp[i] = max3(dp[i], j * dp[i - j], j * (i - j));
+            }
+        }
+
+        return dp[n];
+    }
+
+    private static int max3(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
+
 
     private static void p413() {
         int[] A = {1, 2, 3, 4};
