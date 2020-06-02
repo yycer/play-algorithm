214969042956a19a5dad8e3cc53a3a02a80ef7b8
yaofrankie <15900617310@163.com>
2020-06-02 14:36:36 +0800

    KO 518. Coin Change 2

--- src/main/java/com/frankie/demo/justForFun/Play_23_2020_0602.java
+++ src/main/java/com/frankie/demo/justForFun/Play_23_2020_0602.java
@@ -10,8 +10,33 @@ public class Play_23_2020_0602 {
 
     public static void main(String[] args) {
 //        p474(); // 474. Ones and Zeroes todo: waiting
-        p322(); // 322. Coin Change
+//        p322(); // 322. Coin Change
+        p518(); // 518. Coin Change 2
+    }
 
+    private static void p518() {
+        int[] coins = {1, 2, 5};
+        int amount = 5;
+        int ret1 = change(amount, coins);
+        System.out.println(ret1);
+    }
+
+    private static int change(int amount, int[] coins) {
+        int len = coins.length;
+        int[][] dp = new int[len + 1][amount + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= len; i++){
+            dp[i][0] = 1;
+            for (int a = 1; a <= amount; a++){
+                int rest = a - coins[i - 1];
+                if (rest >= 0){
+                    dp[i][a] = dp[i - 1][a] + dp[i][rest];
+                } else {
+                    dp[i][a] = dp[i - 1][a];
+                }
+            }
+        }
+        return dp[len][amount];
     }
 
     private static void p322() {
