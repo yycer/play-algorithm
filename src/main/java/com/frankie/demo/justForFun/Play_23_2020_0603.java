0b5b2d49e53f8c9e852939e3dd489ed8b6b550c9
yaofrankie <15900617310@163.com>
2020-06-03 13:13:53 +0800

    KO 518. Coin Change 2

--- src/main/java/com/frankie/demo/justForFun/Play_23_2020_0603.java
+++ src/main/java/com/frankie/demo/justForFun/Play_23_2020_0603.java
@@ -9,7 +9,70 @@ import java.util.Arrays;
 public class Play_23_2020_0603 {
 
     public static void main(String[] args) {
-        p322(); // 322. Coin Change
+//        p322(); // 322. Coin Change
+//        p518(); // 518. Coin Change 2
+    }
+
+    private static void p518() {
+//        int[] coins = {1, 2, 5};
+//        int[] coins = {5};
+        int[] coins = {2};
+//        int amount = 5;
+//        int amount = 5;
+        int amount = 3;
+//        int ret1 = change(coins, amount);
+        int ret1 = changeOneDimension(coins, amount);
+        System.out.println(ret1);
+    }
+
+    private static int changeOneDimension(int[] coins, int amount) {
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int c: coins){
+            for (int i = 1; i <= amount; i++){
+                // If bag has some rest.
+                if (i - c >= 0){
+                    dp[i] += dp[i - c];
+                }
+            }
+        }
+        return dp[amount];
+    }
+
+    private static int changeOneDimensionOptimize(int[] coins, int amount) {
+
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+
+        for (int c: coins){
+            for (int i = c; i <= amount; i++){
+                dp[i] += dp[i - c];
+            }
+        }
+        return dp[amount];
+    }
+
+    private static int change(int[] coins, int amount) {
+
+        int len = coins.length;
+        int[][] dp = new int[len + 1][amount + 1];
+        // Think about coins = [], amount = 0.
+        dp[0][0] = 1;
+
+        for (int i = 1; i <= len; i++){
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; j++){
+                // Bag has some rest.
+                if (j - coins[i - 1] >= 0){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[len][amount];
     }
 
     private static void p322() {
