2bf71789aa9e129f0794e6c60fa5dc4b2c4cbc2e
yycer <15900617310@163.com>
2020-06-08 15:07:34 +0800

    309. Best Time to Buy and Sell Stock with Cooldown

--- src/main/java/com/frankie/demo/justForFun/week24/Play_23_2020_0608.java
+++ src/main/java/com/frankie/demo/justForFun/week24/Play_23_2020_0608.java
@@ -10,6 +10,31 @@ public class Play_23_2020_0608 {
 //        p121(); // 121. Best Time to Buy and Sell Stock
 //        p122(); // 122. Best Time to Buy and Sell Stock II
 //        p123(); // 123. Best Time to Buy and Sell Stock III
+//        p309(); // 309. Best Time to Buy and Sell Stock with Cooldown
+    }
+
+    private static void p309() {
+        int[] prices = {1, 2, 3, 0, 2};
+        int a = maxProfit_309(prices);
+        System.out.println(a);
+    }
+
+    private static int maxProfit_309(int[] prices) {
+        int len = prices.length;
+        if (len < 2) return 0;
+        int[] hold   = new int[len];
+        int[] unHold = new int[len];
+
+        hold[0] = -prices[0];
+        hold[1] = -Math.min(prices[0], prices[1]);
+        unHold[1] = Math.max(0, hold[0] + prices[1]);
+
+        for (int i = 2; i < len; i++){
+            hold[i]   = Math.max(hold[i - 1], unHold[i - 2] - prices[i]);
+            unHold[i] = Math.max(unHold[i - 1], hold[i - 1] + prices[i]);
+        }
+
+        return unHold[len - 1];
     }
 
     private static void p123() {
