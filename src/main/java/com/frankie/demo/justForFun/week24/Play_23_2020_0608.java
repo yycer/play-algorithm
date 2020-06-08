7925cfa8b86610a664d2c9a80774b14842fe03f6
yycer <15900617310@163.com>
2020-06-08 11:18:08 +0800

    122. Best Time to Buy and Sell Stock II

--- src/main/java/com/frankie/demo/justForFun/week24/Play_23_2020_0608.java
+++ src/main/java/com/frankie/demo/justForFun/week24/Play_23_2020_0608.java
@@ -7,7 +7,47 @@ package com.frankie.demo.justForFun.week24;
 public class Play_23_2020_0608 {
 
     public static void main(String[] args) {
-        p121(); // 121. Best Time to Buy and Sell Stock
+//        p121(); // 121. Best Time to Buy and Sell Stock
+//        p122(); // 122. Best Time to Buy and Sell Stock II
+    }
+
+    private static void p122() {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        int a = maxProfit_122(prices);
+        System.out.println(a);
+    }
+
+    private static int maxProfit_122(int[] prices) {
+        int len = prices.length;
+        int ans = 0;
+
+        for (int i = 1; i < len; i++){
+            ans += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return ans;
+    }
+
+    private static int maxProfit_122_not_work(int[] prices) {
+
+        int len = prices.length;
+        int[] hold   = new int[len];
+        int[] unhold = new int[len];
+        hold[0] = -prices[0];
+
+        for (int i = 1; i < len; i++){
+            int cur = prices[i];
+            hold[i] = Math.max(-cur, hold[i - 1]);
+
+            // 卖了就赚钱
+            if (cur + hold[i - 1] > 0){
+                unhold[i] = unhold[i - 1] + cur + hold[i - 1];
+                hold[i]   = Integer.MIN_VALUE;
+            } else {
+                unhold[i] = unhold[i - 1];
+            }
+        }
+
+        return unhold[len - 1];
     }
 
     private static void p121() {
