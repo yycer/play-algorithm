a79d6cd28495552394a4686de9fb78897e9ba822
yycer <15900617310@163.com>
2020-06-15 11:47:23 +0800

    1091. Shortest Path in Binary Matrix [DFS Not work]

--- src/main/java/com/frankie/demo/justForFun/week24/Play_23_2020_0611.java
+++ src/main/java/com/frankie/demo/justForFun/week24/Play_23_2020_0611.java
@@ -2,6 +2,7 @@ package com.frankie.demo.justForFun.week24;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -12,7 +13,55 @@ public class Play_23_2020_0611 {
 
     public static void main(String[] args) {
 //        p394(); // 394. Decode String
-        p127(); // 127. Word Ladder
+//        p127(); // 127. Word Ladder
+        p1091(); // 1091. Shortest Path in Binary Matrix
+    }
+
+    private static void p1091() {
+//        int[][] grid = {{0, 1, 1, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 1, 0, 0}};
+        int[][] grid = {{0, 0, 0}, {1, 1, 0}, {1, 1, 0}};
+        shortestPathBinaryMatrix(grid);
+    }
+
+    // not work
+    private static int shortestPathBinaryMatrix(int[][] grid) {
+        int row = grid.length;
+        // if (row == 0) return 0;
+        int[][] dp = new int[row][row];
+
+        // Top-left
+        if (grid[0][0] == 1){
+            return -1;
+        } else if (grid[0][0] == 0){
+            dp[0][0] = 1;
+        }
+
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < row; j++){
+//                if ()
+            }
+        }
+        helper(grid, dp, row, 0, 0);
+        return dp[row - 1][row - 1] == Integer.MAX_VALUE ? -1 : dp[row - 1][row - 1];
+    }
+
+    private static void helper(int[][] grid, int[][] dp, int row, int i, int j){
+
+        for(int m = -1; m <= 1; m++){
+            for (int n = -1; n <= 1; n++){
+                int min = Integer.MAX_VALUE;
+                if (m == 0 && n == 0) continue;
+                int x = i + m;
+                int y = j + n;
+                if (x < 0 || x >= row || y < 0 || y >= row) continue;
+                if (grid[x][y] == 1) continue;
+                grid[i][j] = 1;
+//                if (Math.abs(dp[x][y] - dp[i][j]) <= 1) continue;
+                min = Math.min(min, dp[i][j] + 1);
+                dp[x][y] = min;
+                helper(grid, dp, row, x , y);
+            }
+        }
     }
 
     private static void p127() {
